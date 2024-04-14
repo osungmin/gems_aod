2c42647dd7f840daae3744d80a5b2dc811f01837
Sungmin O <53712038+osungmin@users.noreply.github.com>
2024-04-15 02:57:12 +0900

    Update run_down_era.sh

--- prepdata/era/run_down_era.sh
+++ prepdata/era/run_down_era.sh
@@ -1,13 +1,13 @@
-#select variable name (era5land or era)
+#select variable name (era5land)
 #var=surface_net_solar_radiation
 #var=surface_net_thermal_radiation
 #var=2m_temperature
 #var=2m_dewpoint_temperature
 #var=surface_pressure
 #var='10m_v_component_of_wind'
-#var=boundary_layer_height
+#var=boundary_layer_height #era5
 
-for yr in {2022..2022} ;
+for yr in {2022..2023} ;
   do
 
   for mon in {01..12} ;
@@ -17,8 +17,8 @@ for yr in {2022..2022} ;
      echo ${yr} ${mon} ${var}
 
      #either for era or eaa5land
-     #./down_era.py ${yr} ${mon} ${var}
-     #./down_eraland.py ${yr} ${mon} ${var}
+     ./down_era5.py ${yr} ${mon} ${var}
+     #./down_era5land.py ${yr} ${mon} ${var}
           
      done
 
